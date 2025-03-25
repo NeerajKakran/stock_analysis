295409a67efb42dd3080c00a0f99e2670c8f3456
Krishim5802 <krishim2002@gmail.com>
2025-03-25 14:23:57 +0530

    Add files via upload

---
+++ api/webhook.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+    if (req.method === "POST") {
+        console.log("Received Webhook Data:", req.body);
+        return res.status(200).json({ message: "Webhook received successfully" });
+    } else {
+        return res.status(405).json({ message: "Method Not Allowed" });
+    }
+}
