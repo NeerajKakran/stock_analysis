6576eb9588a5288d5d74afd77cc49dcf877d2f08
Krishim5802 <krishim2002@gmail.com>
2025-03-25 14:36:47 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -2,6 +2,8 @@ export default function handler(req, res) {
     if (req.method === "POST") {
         console.log("Received Webhook Data:", req.body);
         return res.status(200).json({ message: "Webhook received successfully" });
+    } else if (req.method === "GET") {
+        return res.status(200).json({ message: "Webhook is live!" });
     } else {
         return res.status(405).json({ message: "Method Not Allowed" });
     }
