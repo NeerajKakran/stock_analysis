79f55361dc81741cb387ae4668b6bc8496d0483c
NeerajKakran <115134802+NeerajKakran@users.noreply.github.com>
2025-11-27 09:26:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const tableData = [
     {
         name: "Combined Table", // Combines data from multiple sectors
         files: [
-            "Nifty 50 Prediction 2025-11-26.csv",
-            "Nifty Fin Services Prediction 2025-11-26.csv",
-            "Nifty Infra Prediction 2025-11-26.csv",
+            "Nifty 50 Prediction 2025-11-27.csv",
+            "Nifty Fin Services Prediction 2025-11-27.csv",
+            "Nifty Infra Prediction 2025-11-27.csv",
         ],
     },
-    { name: "Nifty IT", file: "Nifty IT Prediction 2025-11-26.csv" },
-    { name: "Nifty Auto", file: "Nifty Auto Prediction 2025-11-26.csv" },
-    { name: "Nifty FMCG", file: "Nifty FMCG Prediction 2025-11-26.csv" },
-    { name: "Nifty Metal", file: "Nifty Metal Prediction 2025-11-26.csv" },
-    { name: "Nifty Bank", file: "Nifty Bank Prediction 2025-11-26.csv" },
+    { name: "Nifty IT", file: "Nifty IT Prediction 2025-11-27.csv" },
+    { name: "Nifty Auto", file: "Nifty Auto Prediction 2025-11-27.csv" },
+    { name: "Nifty FMCG", file: "Nifty FMCG Prediction 2025-11-27.csv" },
+    { name: "Nifty Metal", file: "Nifty Metal Prediction 2025-11-27.csv" },
+    { name: "Nifty Bank", file: "Nifty Bank Prediction 2025-11-27.csv" },
 ];
 
 // Folder path for CSV files
-const folderPath = './2025-11-26/';
+const folderPath = './2025-11-27/';
 
 // Function to render the table layout
 function renderTableLayout() {
@@ -30,7 +30,7 @@ function renderTableLayout() {
     const headerRow = document.createElement('tr');
     const header = document.createElement('th');
     header.setAttribute('colspan', 3); // Adjusting colspan for a 3-column layout
-    header.textContent = "Predictions for 26-November-26";
+    header.textContent = "Predictions for 27-November-27";
     header.style.textAlign = "center";
     headerRow.appendChild(header);
     table.appendChild(headerRow);
@@ -158,6 +158,7 @@ function displayDataInCell(index, data) {
 // Initial render
 renderTableLayout();
 populateTableData();
+
 
 
 
